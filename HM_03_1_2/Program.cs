c9539b8e00a8b01ccee18c725a1f1d58e70128f9
Olga Sh <shei-olya@mail.ru>
2022-08-30 15:38:07 +0300

    Практическоое задание_Задачa№23

---
+++ HM_03_1_2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+Console.WriteLine("Задача 19. Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом");
+
+int[] array = new int[5];
+
+for (int i=0; i<5;i++)
+{
+    Console.WriteLine("Введите число подиндексом" +i);
+    array[i]=int.Parse(Console.ReadLine()); 
+}
+
+if (array[0]==array[4]&& array[1]==array[3])
+{
+Console.WriteLine(array+" число является палиндромом");
+}
+else
+{
+ Console.WriteLine(array+" число не является палиндромом");    
+}
---
+++ HM_03_2/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+Console.WriteLine("Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.");
+
+Console.WriteLine("Введите координату x1");
+int x1= int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координату y1");
+int y1= int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координату z1");
+int z1= int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координату x2");
+int x2= int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координату y2");
+int y2= int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координату z2");
+int z2= int.Parse(Console.ReadLine());
+
+double vec = Math.Sqrt(Math.Pow(x2-x1, 2)+Math.Pow(y2-y1, 2)+Math.Pow(z2-z1, 2));
+Console.WriteLine(vec);
