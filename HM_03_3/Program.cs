c736ad926168da584fa7a499335a51f46da2ed12
Olga Sh <shei-olya@mail.ru>
2022-08-30 15:39:19 +0300

    Практическоое задание_Задачa№23

---
+++ HM_03_3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Задача 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.");
+
+Console.WriteLine("Введите число n");
+double n= double.Parse(Console.ReadLine());
+
+Console.Write(n +"->");
+n = Math.Abs(n);
+
+for ( int i=1; i<=n; i++)
+{
+Console.Write(Math.Pow(i, 3) +",");
+}
