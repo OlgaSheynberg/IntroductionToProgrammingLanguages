e1d070d01f2a400e2b0622e0d9e050682964642f
Olga Sh <shei-olya@mail.ru>
2022-09-04 15:01:56 +0300

    Практическоое задание_Задачa№27

--- HM_4_2/Program.cs
+++ HM_4_2/Program.cs
@@ -1,13 +1,16 @@
 ﻿Console.Clear();
 Console.WriteLine("Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.");
 Console.WriteLine("введите число: ");
-int a = int.Parse(Console.ReadLine());
 
+string chislo=Console.ReadLine();
+int a = int.Parse(chislo);
 int c=0;
+int d=a;
 
-for(int i=1;i<= a.Length; i++)
-
+for(int i=1;i<= chislo.Length; i++)
 {
-c=c+ a%10;
+c=c+d%10;
+d= d/10;
+//Console.WriteLine( c);
 }
 Console.WriteLine("Сумма цифр в числе равна: "+c);
