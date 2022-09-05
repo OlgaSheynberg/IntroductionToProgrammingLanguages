405a7aa0f7ed9c5beb8d90203def08cdf132c7d5
OlgaSheynberg <110896081+OlgaSheynberg@users.noreply.github.com>
2022-09-05 16:17:31 +0300

    Update Program.cs

--- HM_5_1_2/Program.cs
+++ HM_5_1_2/Program.cs
@@ -1,12 +1,11 @@
-﻿//Console.Clear;
+Console.Clear();
 Console.WriteLine("Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.");
 Console.WriteLine("Введите размер массива: ");
-
-int n= int.Parse(Console.ReadLine());
-while(n<0)
+int n =int.Parse(Console.ReadLine()); 
+while (n <0)
 {
-    Console.WriteLine("Введите положительное число: ");
-    int n= int.Parse(Console.ReadLine());
+    Console.WriteLine(" введите положительное число");
+    n = int.Parse(Console.ReadLine());
 }
 
 int count=0;
@@ -14,8 +13,11 @@ int[]array= new int[n];
 for(int i=0; i<n; i++)
 {
 array[i]= new Random().Next(100,1000);
-//count+=array[i]%2=0;
-//Console.Write(Strarray);
 }
-string Strarray = string.Join(",", array);
-Console.Write(Strarray);
+foreach(int e in array)
+{
+    count+=e%2==0?1:0;
+    Console.Write(e+" ");
+}
+
+Console.Write("Количество чётных чисел в массиве: "+count);
